115b1304374c23e202d202c4273300cf4b52d484
xxvigrufv23 <dark9723@gmail.com>
2022-06-28 00:01:55 +0900

    20220627 cjy 인증번호 이메일 전송(1)(2) 추가 작업

--- src/main/java/com/study/controller/MemberController.java
+++ src/main/java/com/study/controller/MemberController.java
@@ -1,8 +1,14 @@
 package com.study.controller;
 
+import java.util.Random;
+
+import javax.mail.internet.MimeMessage;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -28,6 +34,9 @@ public class MemberController {
 	
 	@Setter(onMethod_=@Autowired)
 	public MemberService memberService;
+	
+	@Autowired
+	private JavaMailSender mailSender;
 	
 	
 	
@@ -65,18 +74,57 @@ public class MemberController {
 	public String memberIdChkPOST(String memberId) throws Exception {
 		
 		//logger.info("[API CALLED : /member/memberIdChk ]" );
-		
 		int result = memberService.idCheck(memberId);
-		
+
 		if (result != 0) {
-			
 			return "fail";
-			
 		} else {
-			
 			return "success";
-		
 		}
-		
 	}
+
+	/* 이메일 인증 */
+    @RequestMapping(value="/mailCheck", method=RequestMethod.GET)
+    @ResponseBody
+    public String mailCheckGET(String email) throws Exception{
+        
+        /* 뷰(View)로부터 넘어온 데이터 확인 */
+        logger.info("이메일 데이터 전송 확인");
+        logger.info("인증번호 : " + email);
+                
+        Random randomVerificationNumber = new Random();
+        int checkNum = randomVerificationNumber.nextInt(888888) + 111111;
+        
+        logger.info("인증번호 : " + checkNum);
+        
+        /* 이메일 보내기 */
+        String setFrom = "cjy_calipso@naver.com"; //발신자
+        String toMail = email;                    //수신자
+        String title = "회원가입 인증 이메일 입니다.";    //제목
+        String content = 
+                "홈페이지를 방문해주셔서 감사합니다." +
+                "<br><br>" + 
+                "인증 번호는 " + checkNum + "입니다." + 
+                "<br>" + 
+                "해당 인증번호를 인증번호 확인란에 기입하여 주세요.";
+
+//        try {
+//            MimeMessage message = mailSender.createMimeMessage();
+//            MimeMessageHelper helper = new MimeMessageHelper(message, true, "utf-8");
+//            helper.setFrom(setFrom);
+//            helper.setTo(toMail);
+//            helper.setSubject(title);
+//            helper.setText(content,true);
+//            mailSender.send(message);
+//            
+//        }catch(Exception e) {
+//            e.printStackTrace();
+//        }
+ 
+        //숫자를 문자형으로 변환하는 방법 Integer.toString(숫자A);
+        String num = Integer.toString(checkNum); 
+        
+        return num;
+    }
+   
 }
